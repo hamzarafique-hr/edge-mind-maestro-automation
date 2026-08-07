86b97cc35ac21224160f5ef030ae74ad60206fc0
Hamza Rafique <hamza.rafique@imaginationai.net>
2026-08-07 09:36:10 +0500

    first commit

---
+++ my-maestro-project/elements/login_page.js
@@ -0,0 +1,22 @@
+// elements/login_page.js
+output.loginPage = {
+    vethosButton: "Vethos",     // resource ID
+    homeButton: "Home",
+    agentDownload: "rvMyAgents",
+
+
+
+
+
+
+
+
+
+    // emailField: "email_input_id",      // resource ID
+    // passwordField: "password_input_id",  // resource ID
+    // signInButton: "Sign In",            // Text or accessibility label
+    // errorPopup: "Invalid credentials"   // Text assertion
+    // passwordField: "password_input_id",  // resource ID
+    // signInButton: "Sign In",            // Text or accessibility label
+    // errorPopup: "Invalid credentials"   // Text assertion
+};
---
+++ my-maestro-project/elements/onboarding_elements.js
@@ -0,0 +1,20 @@
+// elements/onboarding_elements.js
+
+output.onboardingPage = {
+    welcomeTitle: "What brings you to Vethos?",
+    justExploringCard: "Just Exploring",
+    nextButton: "Next",
+    skipButton: "Skip",
+    personalAssistantTitle: "Personal Assistant",
+    onDeviceTitle: "100% On-Device",
+    startExploringButton: "Start exploring",
+    brandHeader: "Vethos",
+    workspacePrompt: "What can we do for you?",
+    agentLoadingStatus: "Your agent is suiting up…"
+};
+
+// output.dashboardPage = {
+//     brandHeader: "Vethos",
+//     workspacePrompt: "What can we do for you?",
+//     agentLoadingStatus: "Your agent is suiting up…"
+// };
